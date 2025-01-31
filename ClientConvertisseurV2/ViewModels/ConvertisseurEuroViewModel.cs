7595977e877abe9dadf7121d1ede874f394d0f4c
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-01-31 14:44:36 +0100

    Ajout ViewModels

---
+++ ClientConvertisseurV2/ViewModels/ConvertisseurEuroViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ClientConvertisseurV2.ViewModels
+{
+    public class ConvertisseurEuroViewModel: ObservableObject
+    {
+
+    }
+}
