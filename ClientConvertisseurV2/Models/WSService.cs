71703976c3186073232b26b334e9405e6e3035ee
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-01-31 14:41:47 +0100

    Ajout interface IService, et classes Devise et WSService + Package NuGet MVVM.

---
+++ ClientConvertisseurV2/Models/WSService.cs
@@ -0,0 +1,38 @@
+﻿using ClientConvertisseurV2.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using WSConvertisseur.Models;
+
+namespace ClientConvertisseurV2.Models
+{
+    public class WSService : IService
+    {
+        private HttpClient client = new HttpClient();
+
+        public WSService(string url) 
+        {
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<List<Devise>> GetDevisesAsync(string nomControleur)
+        {
+            try
+            {
+                return await client.GetFromJsonAsync<List<Devise>>(nomControleur);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
